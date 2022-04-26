5acfcc6246d7a24e00d712b1f29aab0c2e1cf9fd
djzhong1 <89283260+djzhong1@users.noreply.github.com>
2022-04-26 15:42:47 +0800

    Add files via upload

---
+++ maskrcnn_benchmark/data/datasets/__init__.py
@@ -0,0 +1,9 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+
+from .synthtext import SynthtextDataset
+from .icdar import IcdarDataset
+__all__ = [
+    "SynthtextDataset",
+    "IcdarDataset",
+    
+]
